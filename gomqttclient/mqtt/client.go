dd362af86ad3f7ca29d8a72cd98d107a2df20ad2
David Christa <davidchrista@gmail.com>
2024-03-05 16:38:08 +0100

    remove hard coded stuff

--- gomqttclient/mqtt/client.go
+++ gomqttclient/mqtt/client.go
@@ -1,36 +1,38 @@
-package mqtt
+package mqttclient
 
 import (
 	"fmt"
 	"log"
 	"math/rand"
-	"os"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-const protocol = "ssl"
-const broker = "v090e996.ala.us-east-1.emqxsl.com"
-const port = 8883
-const topic = "testdata/1"
-const username = "david"
-var password string
+type Config struct {
+	Protocol string
+	Broker string
+	Port int
+	Topic string
+	Username string
+	Password string
+}
 
 var cli mqtt.Client
+var topic string
 
-func InitClient() {
-	password = os.Getenv("MQTT_PASSWORD")
+func InitClient(c Config) {
+	topic = c.Topic
 
-	connectAddress := fmt.Sprintf("%s://%s:%d", protocol, broker, port)
+	connectAddress := fmt.Sprintf("%s://%s:%d", c.Protocol, c.Broker, c.Port)
 	rand.New(rand.NewSource(time.Now().UnixNano()))
 	clientID := fmt.Sprintf("go-client-%d", rand.Int())
 
 	fmt.Println("connect address: ", connectAddress)
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(connectAddress)
-	opts.SetUsername(username)
-	opts.SetPassword(password)
+	opts.SetUsername(c.Username)
+	opts.SetPassword(c.Password)
 	opts.SetClientID(clientID)
 	opts.SetKeepAlive(time.Second * 60)
 
